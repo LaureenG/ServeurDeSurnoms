6d3e2e7a3753d13978816344833a363bb53d7188
Laureen Ginier <ginier.laureen@etu.unice.fr>
2014-11-10 16:38:09 +0100

    Amélioration de la classe serveurThread

--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -5,18 +5,22 @@ import java.net.ServerSocket;
 
 public class Serveur {
     public static void main(String[] args) throws IOException {
+        // Création du socket d'écoute sur le port 1234
         int portEcoute = 1234;
         ServerSocket serverSocket = null;
-        boolean listening = true;
         try {
             serverSocket = new ServerSocket(portEcoute);
         } catch (IOException e) {
             System.err.println("Could not listen on port: 1234.");
             System.exit(-1);
         }
+        // Attente de connexions
+        boolean listening = true;
         while (listening) {
+            // Création d'une thread pour gérer le client
             new ServeurThread(serverSocket.accept()).start();
         }
+        // Fermeture du socket d'écoute
         serverSocket.close();
     }
 }
--- src/serveur/ServeurThread.java
+++ src/serveur/ServeurThread.java
@@ -1,4 +1,5 @@
 package serveur;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
@@ -12,6 +13,7 @@ import java.util.Set;
 
 import data.Data;
 
+import sds.ErrorCode;
 import sds.Query;
 import sds.Reply;
 
@@ -23,73 +25,111 @@ public class ServeurThread extends Thread {
         this.socket = socket;
     }
 
+    private Query reception(ObjectInputStream in)
+            throws ClassNotFoundException, IOException {
+        System.out.print("Reception ... \t\t");
+        return (Query) in.readObject();
+    }
+
+    private void envoi(Reply reply, ObjectOutputStream out) throws IOException {
+        System.out.print("Envoi ... \t\t");
+        out.writeObject(reply);
+        out.flush();
+        System.out.println("[OK]");
+    }
+
+    private Reply execution(Query query) {
+        System.out.println("Dépaquetage ...");
+        int numeroService = query.getService();
+        int codeErr = ErrorCode.IM_A_TEAPOT;
+        Object obj = null;
+        codeErr = appelService(query, obj);
+        System.out.println("Paquetage ...");
+        if (codeErr < 0) {
+            return null;
+        } else {
+            return new Reply(numeroService, codeErr, obj, query.getQueryID());
+        }
+    }
+
+    private int appelService(Query query, Object obj) {
+        Data service = Data.getInstance();
+        Set<String> set = new HashSet<String>();
+        int codeErr = -1;
+        switch (query.getService()) {
+        case 1:
+            codeErr = service.putSurname(query.getArg1(), query.getArg2());
+            break;
+        case 2:
+            codeErr = service.postName(query.getArg1(), query.getArg2());
+            break;
+        case 3:
+            codeErr = service.postSurname(query.getArg1(), query.getArg2());
+            break;
+        case 4:
+            HashMap<String, List<String>> map = new HashMap<String, List<String>>();
+            codeErr = service.getAll(map);
+            obj = map;
+            break;
+        case 5:
+            codeErr = service.getName(set);
+            break;
+        case 6:
+            codeErr = service.getSurname(set, query.getArg1());
+            break;
+        case 7:
+            codeErr = service.deleteSurname(query.getArg1());
+            break;
+        case 8:
+            codeErr = service.deleteName(query.getArg1());
+            break;
+        }
+        return codeErr;
+    }
+
     public void run() {
-    	System.out.println("Nouveau client!");
+        System.out.println("Nouveau client!");
+        ObjectOutputStream out;
+        ObjectInputStream in;
         try {
-            ObjectOutputStream out = new ObjectOutputStream(
-                    new BufferedOutputStream(socket.getOutputStream()));
-            ObjectInputStream in = new ObjectInputStream(
-                    new BufferedInputStream(socket.getInputStream()));
-            boolean open = true;
-            while (open) {
-            	System.out.print("Reception ... \t\t");
-                Query query = (Query) in.readObject();
-                System.out.println("[OK]");
-                System.out.println("Dépaquetage ...");
-                int numeroService = query.getService();
-                int codeErr = 418;
-                Object obj = null;
-                Data service = Data.getInstance();
-                Set<String> set;
-                switch (numeroService) {
-                case 0:
-                    open = false;
-                    codeErr = 200;
-                    break;
-                case 1:
-                    codeErr = service.putSurname(query.getArg1(),query.getArg2());
-                    break;
-                case 2:
-                     codeErr = service.postName(query.getArg1(), query.getArg2());
-                    break;
-                case 3:
-                     codeErr = service.postSurname(query.getArg1(), query.getArg2());
-                    break;
-                case 4:
-                    HashMap<String, List<String>> map = new HashMap<String, List<String>>();
-                     codeErr = service.getAll(map);
-                     obj = map;
-                    break;
-                case 5:
-                   set = new HashSet<String>();
-                    codeErr = service.getName(set);
-                    break;
-                case 6:
-                    set = new HashSet<String>();
-                    codeErr = service.getSurname(set, query.getArg1());
-                    break;
-                case 7:
-                    codeErr = service.deleteSurname(query.getArg1());
-                    break;
-                case 8:
-                    codeErr = service.deleteName(query.getArg1());
-                    break;
-                }
-                System.out.println("Paquetage ...");
-                Reply reply = new Reply(numeroService, codeErr, obj,
-                        query.getQueryID());
-                System.out.print("Envoie ... \t\t");
-                out.writeObject(reply);
-                out.flush();
-                System.out.println("[OK]");
+            out = new ObjectOutputStream(new BufferedOutputStream(
+                    socket.getOutputStream()));
+            in = new ObjectInputStream(new BufferedInputStream(
+                    socket.getInputStream()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        boolean open = true;
+        while (open) {
+            Query query;
+            try {
+                query = reception(in);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                return;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
             }
-            System.out.println("CLOSE");
+            Reply reply = execution(query);
+            if (reply != null) {
+                try {
+                    envoi(reply, out);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                open = false;
+            }
+        }
+        System.out.println("CLOSE");
+        try {
             out.close();
             in.close();
             socket.close();
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
